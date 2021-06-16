c226e7c894ef21e775ae4d411e00e1e939dcf845
Victor Freire <victor@freire.dev.br>
2021-06-16 15:57:27 -0300

    downgrade to nixos-21.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
     home-manager = {
       url = "github:nix-community/home-manager/e92f5bb79e8c72b6afd64d3b6e4614669fcc1518";
       inputs.nixpkgs.follows = "nixpkgs";
