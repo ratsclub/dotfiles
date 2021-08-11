0e14991591282a68a337a807ba81e9376ce0d756
Victor Freire <victor@freire.dev.br>
2021-08-11 13:46:25 -0300

    remove i3wm

--- hosts/earth/default.nix
+++ hosts/earth/default.nix
@@ -39,18 +39,6 @@
       enable = true;
       wayland = true;
     };
-
-    # Enable i3 window manager
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu #application launcher most people use
-        i3status # gives you the default i3 status bar
-        i3blocks
-        i3lock #default i3 screen locker
-        i3blocks #if you are planning on using i3blocks over i3status
-      ];
-    };
   };
 
   environment = {
