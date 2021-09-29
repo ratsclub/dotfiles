389d043b694e8d7e4c42fa62fbc19040fc02a4ec
Victor Freire <victor@freire.dev.br>
2021-09-29 20:47:47 -0300

    added networkmanager group

--- hosts/t495/default.nix
+++ hosts/t495/default.nix
@@ -62,10 +62,9 @@
     };
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.victor = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "docker" "networkmanager" ];
     password = "changeme";
   };
 
