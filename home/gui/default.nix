747b968ab4e82d7fea9388feedb47f4331a6314d
Victor Freire <victor@freire.dev.br>
2021-09-29 21:31:23 -0300

    added foliate

--- home/gui/default.nix
+++ home/gui/default.nix
@@ -2,14 +2,20 @@
 
 {
   home.packages = with pkgs; [
+    # chat
     discord
     tdesktop
-    
+
+    # editors
+    jetbrains.idea-community
+    jetbrains.pycharm-community
+
+    # ebooks
+    foliate
+
+    # misc
     bitwarden
     firefox
     obsidian
-
-    jetbrains.idea-community
-    jetbrains.pycharm-community
   ];
 }
