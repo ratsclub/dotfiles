13bc878ca78f8b862da57bdd6f4e95abc48fb2a7
Victor Freire <victor@freire.dev.br>
2021-10-11 14:56:17 -0300

    fix neovim config

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -13,48 +13,18 @@
 
     plugins = with pkgs.vimPlugins; [
       {
-        plugin = vim-polyglot;
-        config = ''
-          " Find files using Telescope command-line sugar.
-          nnoremap <leader>ff <cmd>Telescope find_files<cr>
-          nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-          nnoremap <leader>fb <cmd>Telescope buffers<cr>
-          nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-        '';
+        plugin = telescope-nvim;
       }
       {
         plugin = vim-noctu;
-        config = ''
-          " color scheme
-          colorscheme noctu
-        '';
       }
       {
         plugin = ale;
-        config = ''
-          let g:ale_completion_enabled = 1
-          let g:ale_linters = {"c": ["clang"], "rust": ["analyzer", "cargo"]}
-          let g:ale_fixers = {"rust": ["rustfmt"], "sql": ["pgformatter"], "nix": ["nixfmt"], "json": ["jq"]}
-          let g:ale_rust_analyzer_config = {'checkOnSave': {'command': 'clippy', 'enable': v:true}}
-        '';
       }
       
       vim-polyglot
     ];
 
-    extraConfig = ''
-      " show numbers
-      set nu
-
-      " show relative numbers
-      set rnu
-
-      " space as leader key
-      nnoremap <space> <nop>
-      let mapleader=" "
-
-      " escape key
-      inoremap jk <Esc>
-    '';
+    extraConfig = builtins.readFile ./config.vim;
   };
 }
