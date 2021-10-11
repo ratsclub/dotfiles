ea3482fe33dcd8491834655fb41051805d1df487
Victor Freire <victor@freire.dev.br>
2021-10-11 14:31:25 -0300

    added ale lsp

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -11,13 +11,50 @@
     withPython3 = true;
     withRuby = true;
 
-    # package = pkgs.neovim;
     plugins = with pkgs.vimPlugins; [
-      telescope-nvim
+      {
+        plugin = vim-polyglot;
+        config = ''
+          " Find files using Telescope command-line sugar.
+          nnoremap <leader>ff <cmd>Telescope find_files<cr>
+          nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+          nnoremap <leader>fb <cmd>Telescope buffers<cr>
+          nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+        '';
+      }
+      {
+        plugin = vim-noctu;
+        config = ''
+          " color scheme
+          colorscheme noctu
+        '';
+      }
+      {
+        plugin = ale;
+        config = ''
+          let g:ale_completion_enabled = 1
+          let g:ale_linters = {"c": ["clang"], "rust": ["analyzer", "cargo"]}
+          let g:ale_fixers = {"rust": ["rustfmt"], "sql": ["pgformatter"], "nix": ["nixfmt"], "json": ["jq"]}
+          let g:ale_rust_analyzer_config = {'checkOnSave': {'command': 'clippy', 'enable': v:true}}
+        '';
+      }
+      
       vim-polyglot
-      vim-noctu
     ];
 
-    extraConfig = builtins.readFile ./config.vim;
+    extraConfig = ''
+      " show numbers
+      set nu
+
+      " show relative numbers
+      set rnu
+
+      " space as leader key
+      nnoremap <space> <nop>
+      let mapleader=" "
+
+      " escape key
+      inoremap jk <Esc>
+    '';
   };
 }
