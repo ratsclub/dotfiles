7328df8d71ad6b6ff6cd0a9877c34000bdbfc72e
Victor Freire <victor@freire.dev.br>
2021-10-11 14:31:34 -0300

    remove binfmt

--- hosts/t495/default.nix
+++ hosts/t495/default.nix
@@ -12,7 +12,6 @@
   device.type = "graphical";
 
   boot = {
-    binfmt.emulatedSystems = [ "aarch64-linux" ];
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
