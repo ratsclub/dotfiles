fd41ed7d3e6a37d2b9610dfc2f529311935c8556
Victor Freire <victor@freire.dev.br>
2021-10-23 12:33:51 -0300

    added go template

--- templates/default.nix
+++ templates/default.nix
@@ -4,4 +4,10 @@
     description = "Rust Project Template";
     path = ./rust;
   };
+
+  # nix flake init -t github:ratsclub/dotfiles#templates.rust
+  go = {
+    description = "Go Project Template";
+    path = ./go;
+  };
 }
