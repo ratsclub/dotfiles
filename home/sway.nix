30a0c728efb63c88121aca3dfb022bd0c931dcf1
Victor Freire <victor@freire.dev.br>
2022-01-04 20:05:05 -0300

    added scaling

--- home/sway.nix
+++ home/sway.nix
@@ -60,6 +60,11 @@ in
         };
       };
       bars = [ ];
+      output = {
+        "HDMI-A-1" = {
+          scale = "1.50";
+        };
+      };
       startup = [
         # Set wallpaper
         { command = "${swaybg} -i ${wallpaper} -m fill"; }
@@ -80,21 +85,7 @@ in
         "Control+${modifier}+Print" = "exec ${grimshot} --notify copy window";
 
         # Lockscreen
-        "${modifier}+Control+l" = ''
-          exec ${swaylock} --screenshots \
-          	--clock \
-          	--indicator \
-          	--indicator-radius 100 \
-          	--indicator-thickness 7 \
-          	--effect-blur 7x5 \
-          	--effect-vignette 0.5:0.5 \
-          	--ring-color bb00cc \
-          	--key-hl-color 880033 \
-          	--line-color 00000000 \
-          	--inside-color 00000088 \
-          	--separator-color 00000000 \
-          	--fade-in 0.2
-        '';
+        "${modifier}+Control+l" = "exec ${swaylock} --screenshots";
 
         # Volume
         "XF86AudioRaiseVolume" =
