367bc346df5fa693c4177767eaf2fef2a078aa1a
Victor Freire <victor@freire.dev.br>
2022-01-04 19:16:15 -0300

    added mpv to newsboat

--- home/newsboat.nix
+++ home/newsboat.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 
+let
+  w3m = "${pkgs.w3m}/bin/w3m";
+  mpv = "${pkgs.mpv}/bin/mpv";
+in
 {
   programs.newsboat = {
     enable = true;
@@ -14,11 +18,19 @@
         tags = [ "tech" "go" ];
         title = "The Go Programming Language Blog";
       }
+      {
+        url = "https://www.youtube.com/feeds/videos.xml?channel_id=UC9-y-6csu5WGm29I7JiwpnA";
+        tags = [ "tech" "youtube" ];
+        title = "Computerphile";
+      }
     ];
     extraConfig = ''
-      html-renderer      "${pkgs.w3m}/bin/w3m -dump -T text/html"
+      html-renderer      "${w3m} -dump -T text/html"
       refresh-on-startup yes
       text-width         72
+
+      # open video on mpv
+      macro v set browser "${mpv} %u" ; open-in-browser ; set browser "${w3m} %u"
     '';
   };
 }
