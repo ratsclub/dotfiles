a3ae8518148ce1861700de5dc15f4b9d7bde2e6d
Victor Freire <victor@freire.dev.br>
2022-01-04 16:27:29 -0300

    added swaylock

--- home/sway.nix
+++ home/sway.nix
@@ -79,6 +79,23 @@ in
         "${modifier}+Print" = "exec ${grimshot} --notify copy active";
         "Control+${modifier}+Print" = "exec ${grimshot} --notify copy window";
 
+        # Lockscreen
+        "${modifier}+Control+l" = ''
+          exec ${swaylock} --screenshots \
+          	--clock \
+          	--indicator \
+          	--indicator-radius 100 \
+          	--indicator-thickness 7 \
+          	--effect-blur 7x5 \
+          	--effect-vignette 0.5:0.5 \
+          	--ring-color bb00cc \
+          	--key-hl-color 880033 \
+          	--line-color 00000000 \
+          	--inside-color 00000088 \
+          	--separator-color 00000000 \
+          	--fade-in 0.2
+        '';
+
         # Volume
         "XF86AudioRaiseVolume" =
           "exec ${pactl} set-sink-volume @DEFAULT_SINK@ +5%";
