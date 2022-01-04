3ff8d1b2b9fda0e7c6bdf5a3c6b223f9e3cb67ae
Victor Freire <victor@freire.dev.br>
2022-01-04 20:05:14 -0300

    added new feeds to newsboat

--- home/newsboat.nix
+++ home/newsboat.nix
@@ -9,19 +9,48 @@ in
     enable = true;
     urls = [
       {
+        title = "Drew DeVault's";
         url = "https://drewdevault.com/blog/index.xml";
         tags = [ "tech" ];
-        title = "Drew DeVault's blog";
       }
       {
+        title = "The Go Programming Language Blog";
         url = "https://go.dev/blog/feed.atom?format=xml";
         tags = [ "tech" "go" ];
-        title = "The Go Programming Language Blog";
+      }
+
+      # Aggregators
+      {
+        title = "Lobste.rs - Nix/Go";
+        url = "https://lobste.rs/t/nix,go.rss";
+        tags = [ "tech" "go" "nix" ];
       }
       {
+        title = "Lobste.rs - Frontpage";
+        url = "https://lobste.rs/rss";
+        tags = [ "tech" ];
+      }
+      {
+        title = "Hacker News - Frontpage";
+        url = "https://news.ycombinator.com/rss";
+        tags = [ "tech" ];
+      }
+
+      # Youtube
+      {
+        title = "Computerphile";
         url = "https://www.youtube.com/feeds/videos.xml?channel_id=UC9-y-6csu5WGm29I7JiwpnA";
         tags = [ "tech" "youtube" ];
-        title = "Computerphile";
+      }
+      {
+        title = "Channel 5 with Andrew Callaghan";
+        url = "https://www.youtube.com/feeds/videos.xml?channel_id=UC-AQKm7HUNMmxjdS371MSwg";
+        tags = [ "entertainment" "youtube" ];
+      }
+      {
+        title = "Luke Smith";
+        url = "https://www.youtube.com/feeds/videos.xml?channel_id=UC2eYFnH61tmytImy1mTYvhA";
+        tags = [ "entertainment" "tech" "youtube" ];
       }
     ];
     extraConfig = ''
