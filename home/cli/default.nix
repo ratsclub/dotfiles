29a68af068b76408df346d49749ca04202a712be
Victor Freire <victor@freire.dev.br>
2022-01-06 13:37:35 -0300

    removed unused settings on git delta

--- home/cli/default.nix
+++ home/cli/default.nix
@@ -66,19 +66,10 @@
         enable = true;
         options = {
           features = "side-by-side line-numbers decorations";
-          syntax-theme = "GitHub";
-          decorations = {
-            commit-decoration-style = "bold yellow box ul";
-            file-style = "bold yellow ul";
-            file-decoration-style = "none";
-            hunk-header-decoration-style = "cyan box ul";
-          };
           delta = {
             navigate = true;
           };
           line-numbers = {
-            line-numbers-left-style = "cyan";
-            line-numbers-right-style = "cyan";
             line-numbers-minus-style = 124;
             line-numbers-plus-style = 28;
           };
