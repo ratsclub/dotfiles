5bcaa9001352e4c0528c0ba4e9b90f1c3d5b1c07
Victor Freire <victor@freire.dev.br>
2022-01-19 18:19:51 -0300

    added basic template

---
+++ templates/basic/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    { } // utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [ hello ];
+        };
+      });
+}
