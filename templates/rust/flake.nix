af0611d42a3182679a782128f0730030ab93f291
Victor Freire <victor@freire.dev.br>
2022-02-19 22:30:42 -0300

    refactor: remove naersk and use buildRustPackage

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -2,62 +2,64 @@
   description = "Rust Project Template.";
 
   inputs = {
-    # TODO remove naers and use pkgs.rustPlatform
-    naersk.url = "github:nmattia/naersk";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rust.url = "github:oxalica/rust-overlay";
+    rustOverlay.url = "github:oxalica/rust-overlay";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, rust }:
-    utils.lib.eachDefaultSystem
-      (
-        system:
-        let
-          project-name = "project-name";
-          rust-channel = "stable";
-          rust-version = "latest"; # ex. 1.55.0
-          rust-overlay = import rust;
+  outputs = { self, nixpkgs, utils, rustOverlay }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        projectName = "project";
 
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ rust-overlay ];
-          };
+        rustChannel = "stable";
+        rustVersion = "latest"; # ex. 1.58.0
 
-          rust-toolchain = pkgs.rust-bin."${rust-channel}"."${rust-version}".default.override {
-            extensions = [
-              "rust-std"
-              "rust-src"
-            ];
-          };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rustOverlay) ];
+        };
 
-          naersk-lib = naersk.lib."${system}".override {
-            rustc = rust-toolchain;
-          };
+        inherit (pkgs)
+          lib
+          mkShell
+          rust-analyzer
+          rust-bin
+          rustPlatform;
 
-          project-pkg = naersk-lib.buildPackage {
-            pname = project-name;
-            root = pkgs.lib.cleanSource ./.;
-          };
-        in
-        rec {
-          # `nix build`
-          packages."${project-name}" = project-pkg;
-          defaultPackage = packages."${project-name}";
+        rustToolchain = rust-bin."${rustChannel}"."${rustVersion}".default.override {
+          extensions = [
+            "rust-std"
+            "rust-src"
+          ];
+        };
 
-          # `nix run`
-          apps."${project-name}" = utils.lib.mkApp {
-            drv = packages."${project-name}";
-          };
-          defaultApp = apps."${project-name}";
+        projectPkg = rustPlatform.buildRustPackage {
+          name = projectName;
+          src = lib.cleanSource ./.;
 
-          # `nix develop`
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              rust-toolchain
-              rust-analyzer
-            ];
-          };
-        }
-      );
+          rustc = "${rustToolchain}/bin/rustc";
+
+          cargoSha256 = lib.fakeSha256;
+        };
+      in
+      rec {
+        # `nix build`
+        packages."${projectName}" = projectPkg;
+        defaultPackage = packages."${projectName}";
+
+        # `nix run`
+        apps."${projectName}" = utils.lib.mkApp {
+          drv = packages."${projectName}";
+        };
+        defaultApp = apps."${projectName}";
+
+        # `nix develop`
+        devShell = mkShell {
+          nativeBuildInputs = [
+            rustToolchain
+            rust-analyzer
+          ];
+        };
+      });
 }
