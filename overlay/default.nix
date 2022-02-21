b54d439169a31157e7e38cf4d57e00f6fcb61084
Victor Freire <victor@freire.dev.br>
2022-02-21 11:09:56 -0300

    fix(pyright): remove pyright overlay

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,24 +1,4 @@
 final: prev: rec {
-  vscode-extensions = prev.vscode-extensions // {
-    # FIXME https://nixpk.gs/pr-tracker.html?pr=160535
-    ms-pyright.pyright = prev.vscode-utils.buildVscodeMarketplaceExtension {
-      mktplcRef = {
-        name = "pyright";
-        publisher = "ms-pyright";
-        version = "1.1.222";
-        sha256 = "sha256-QMX/SawDEnG1xVrug8mvN7EvRrRDkJffcXBUFpQi1XE=";
-      };
-      meta = with final.lib; {
-        description = "VS Code static type checking for Python";
-        downloadPage = "https://marketplace.visualstudio.com/items?itemName=ms-pyright.pyright";
-        homepage = "https://github.com/Microsoft/pyright#readme";
-        changelog = "https://marketplace.visualstudio.com/items/ms-pyright.pyright/changelog";
-        license = licenses.mit;
-        maintainers = with maintainers; [ ratsclub ];
-      };
-    };
-  };
-
   hut = prev.buildGoModule rec {
     pname = "hut";
     version = "ca4420d992f2e7653277d466dfcd1341f4c1f916";
