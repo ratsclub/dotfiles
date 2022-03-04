4fdb72e15f00b8734ebb4b18ac55bf60fe8721db
Victor Freire <victor@freire.dev.br>
2022-03-03 23:26:35 -0300

    feat(notmuch): added automatic tagging

--- home/email.nix
+++ home/email.nix
@@ -1,19 +1,29 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 let
   inherit (config.meta) email name;
+  notmuch = "${pkgs.notmuch}/bin/notmuch";
 in
 {
-  programs.mbsync.enable = true;
-  programs.msmtp.enable = true;
-  programs.notmuch = {
-    enable = true;
-    hooks = {
-      preNew = "mbsync --all";
+  programs = {
+    mbsync.enable = true;
+    msmtp.enable = true;
+    notmuch = {
+      enable = true;
+      new.tags = [ "unread" "inbox" ];
+      hooks = {
+        preNew = "mbsync --all";
+        postNew = ''
+          ${notmuch} tag +uber   -- tag:unread and from:noreply@uber.com
+          ${notmuch} tag +nixos  -- tag:unread and from:discourse@discourse.nixos.org
+          ${notmuch} tag +github -- tag:unread and from:notifications@github.com
+        '';
+      };
     };
   };
+
   accounts.email = {
-    accounts.mailbox = {
+    accounts.personal = {
       realName = name;
       address = email;
       userName = email;
