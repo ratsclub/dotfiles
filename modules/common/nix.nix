5fa7d554b75c32d52ae20f43e62c16fc5766fc58
Victor Freire <victor@freire.dev.br>
2022-10-13 10:35:33 -0300

    feat: add nix module

---
+++ modules/common/nix.nix
@@ -0,0 +1,21 @@
+{ config, inputs, pkgs, ... }:
+
+{
+  nix = {
+    package = pkgs.nix;
+    nixPath = [
+      "nixpkgs=${inputs.nixpkgs}"
+    ];
+
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 1w";
+    };
+
+    registry.nixpkgs.flake = inputs.nixpkgs;
+    settings = {
+      experimental-features = "nix-command flakes";
+      auto-optimise-store = true;
+    };
+  };
+}
