8d78f8e190c588306d38815ca2d45682fda42e8c
Victor Freire <victor@freire.dev.br>
2023-04-24 12:47:35 -0300

    nix(overlay): remove ionide overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,11 +5,5 @@
       inherit (final.lib.attrsets) recursiveUpdate;
     in
     {
-      vscode-extensions = recursiveUpdate prev.vscode-extensions {
-        ionide.ionide-fsharp = prev.vscode-extensions.ionide.ionide-fsharp.overrideAttrs (oldAttrs: rec {
-          version = "7.5.1";
-          sha256 = "sha256-AiDYqYF+F69O/aeolIEzqLmg20YN/I4EV6XMa8UgMns=";
-        });
-      };
     };
 }
