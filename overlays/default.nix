15e171e88dd15079aa62b6573ff8bd4dfa0d8c8e
Victor Freire <victor@freire.dev.br>
2023-04-26 23:16:39 -0300

    nix(overlay): add vscode-extensions overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,13 +3,32 @@
   modifications = final: prev:
     let
       inherit (final.lib.attrsets) recursiveUpdate;
+      inherit (final.vscode-utils) buildVscodeMarketplaceExtension;
     in
     {
-      vscode-extensions = recursiveUpdate prev.vscode-extensions {
-        ionide.ionide-fsharp = prev.vscode-extensions.ionide.ionide-fsharp.overrideAttrs (oldAttrs: rec {
-          version = "7.5.1";
-          sha256 = "sha256-AiDYqYF+F69O/aeolIEzqLmg20YN/I4EV6XMa8UgMns=";
-        });
-      };
+      vscode-extensions = recursiveUpdate prev.vscode-extensions
+        {
+          ionide.ionide-fsharp = buildVscodeMarketplaceExtension {
+            mktplcRef = {
+              name = "Ionide-fsharp";
+              publisher = "Ionide";
+              version = "7.5.3";
+              sha256 = "sha256-y3vb5Zte/fyESC63NqHCBeZ4yzE1EmseGGtHWrYGnv4=";
+            };
+          };
+
+          eamodio.gitlens = buildVscodeMarketplaceExtension {
+            mktplcRef = {
+              name = "gitlens";
+              publisher = "eamodio";
+              # Stable versions are listed on the GitHub releases page and use a
+              # semver scheme, contrary to preview versions which are listed on
+              # the VSCode Marketplace and use a calver scheme. We should avoid
+              # using preview versions, because they expire after two weeks.
+              version = "13.4.0";
+              sha256 = "sha256-CYI62sWPlJNRP2KIkg4vQutIMC6gaCxtTVoOWZIS8Lw=";
+            };
+          };
+        };
     };
 }
