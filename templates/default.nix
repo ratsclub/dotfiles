198718f429e6b0f22e7e1eea6099423b7a2137a9
Victor Freire <victor@freire.dev.br>
2023-08-22 12:43:15 -0300

    templates: remove fsharp

--- templates/default.nix
+++ templates/default.nix
@@ -8,9 +8,4 @@
     description = "Go project template";
     path = ./go;
   };
-
-  fsharp = {
-    description = "F# project template";
-    path = ./fsharp;
-  };
 }
---
+++ templates/dotnet/README.md
@@ -0,0 +1,3 @@
+# .NET template
+
+Run `nix develop --impure` to have dotnet on your PATH.
