fe0f276126b30f18f35123b0a3da504b567890b2
Victor Freire <victor@freire.dev.br>
2024-01-18 12:18:44 -0300

    vscodium: update ionide

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,21 +12,8 @@
             mktplcRef = {
               name = "Ionide-fsharp";
               publisher = "Ionide";
-              version = "7.16.1";
-              sha256 = "sha256-bqpZu6RVVCky0COYi3PmvT0Iz1mrtl6pXMpwFp1eW6k=";
-            };
-          };
-
-          eamodio.gitlens = buildVscodeMarketplaceExtension {
-            mktplcRef = {
-              name = "gitlens";
-              publisher = "eamodio";
-              # Stable versions are listed on the GitHub releases page and use a
-              # semver scheme, contrary to preview versions which are listed on
-              # the VSCode Marketplace and use a calver scheme. We should avoid
-              # using preview versions, because they expire after two weeks.
-              version = "13.4.0";
-              sha256 = "sha256-CYI62sWPlJNRP2KIkg4vQutIMC6gaCxtTVoOWZIS8Lw=";
+              version = "7.17.0";
+              sha256 = "sha256-CC6ySeuO61O/mAkQYGoK/1cd4hlyS0vG+Lqv0HQ7K6c==";
             };
           };
         };
