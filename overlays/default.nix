83e9b941490f429fa31b888da38632be97537ecd
Victor Freire <victor@freire.dev.br>
2024-05-13 13:42:18 -0300

    chore: remove unused overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,21 +1,4 @@
 {
   default = final: _prev: import ../pkgs { pkgs = final; };
-  modifications = final: prev:
-    let
-      inherit (final.lib.attrsets) recursiveUpdate;
-      inherit (final.vscode-utils) buildVscodeMarketplaceExtension;
-    in
-    {
-      vscode-extensions = recursiveUpdate prev.vscode-extensions
-        {
-          ionide.ionide-fsharp = buildVscodeMarketplaceExtension {
-            mktplcRef = {
-              name = "Ionide-fsharp";
-              publisher = "Ionide";
-              version = "7.17.0";
-              sha256 = "sha256-CC6ySeuO61O/mAkQYGoK/1cd4hlyS0vG+Lqv0HQ7K6c==";
-            };
-          };
-        };
-    };
+  modifications = final: prev: { };
 }
