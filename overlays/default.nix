4990904572ded6d6bdebe775b0057ee4fbbc8244
Victor Freire <victor@freire.dev.br>
2024-10-16 17:39:30 -0300

    overlay: remove jetbrains

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,16 +1,12 @@
 { nixpkgs, small }:
 
 let
-  smallPkgs = system: import small {
+  _smallPkgs = system: import small {
     inherit system;
     config.allowUnfree = true;
   };
 in
 {
   default = final: _prev: import ../pkgs { pkgs = final; };
-  modifications = final: prev: {
-    # TODO: wait this to get to unstable
-    # https://nixpk.gs/pr-tracker.html?pr=334777
-    jetbrains = (smallPkgs final.system).jetbrains;
-  };
+  modifications = final: prev: { };
 }
