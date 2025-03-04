53667190abae57090c72b2f5dfecc1e8e528522c
Victor Freire <victor@freire.dev.br>
2025-03-04 09:25:40 -0300

    bash: update bashrc and bash_profile

--- home/modules/bash.nix
+++ home/modules/bash.nix
@@ -35,40 +35,52 @@
       mknote = ''echo -e "---\ntitle:\ndate: $(date -u -Iseconds)\n---" > $(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1).md'';
     };
 
-    bashrcExtra = ''
-      # If not running interactively, don't do anything
-      case $- in
-        *i*) ;;
-        *) return;;
-      esac
+    bashrcExtra =
+      ''
+        # Source global definitions
+          if [ -f /etc/bashrc ]; then
+            . /etc/bashrc
+          fi
 
-      parse_git_branch() {
-        git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-      }
-      export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\e[91m\]\$(parse_git_branch)\[\033[00m\]\$ "
-
-      # colored GCC warnings and errors
-      export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-
-      if ! shopt -oq posix; then
-        if [ -f /usr/share/bash-completion/bash_completion ]; then
-          . /usr/share/bash-completion/bash_completion
-        elif [ -f /etc/bash_completion ]; then
-          . /etc/bash_completion
+        # User specific environment
+        if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
+          PATH="$HOME/.local/bin:$HOME/bin:$PATH"
         fi
-      fi
-    '';
+        export PATH
+
+        # User specific aliases and functions
+        if [ -d ~/.bashrc.d ]; then
+          for rc in ~/.bashrc.d/*; do
+            if [ -f "$rc" ]; then
+              . "$rc"
+            fi
+          done
+        fi
+        unset rc
+
+        # colored GCC warnings and errors
+        export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+        if ! shopt -oq posix; then
+          if [ -f /usr/share/bash-completion/bash_completion ]; then
+            . /usr/share/bash-completion/bash_completion
+          elif [ -f /etc/bash_completion ]; then
+            . /etc/bash_completion
+          fi
+        fi
+      '';
 
     profileExtra = ''
-      [ -f $HOME/.nix-profile/etc/profile.d/nix.sh ] && . $HOME/.nix-profile/etc/profile.d/nix.sh
+      # Get the aliases and functions
+      if [ -f ~/.bashrc ]; then
+        . ~/.bashrc
+      fi
 
       # useful for showing icons on non-NixOS systems
       export XDG_DATA_DIRS=$HOME/.nix-profile/share:$XDG_DATA_DIRS
 
       [ -d "$HOME/.local/bin" ] && export PATH=$PATH:$HOME/.local/bin
-      [ -d "$HOME/.poetry/bin" ] && export PATH=$PATH:$HOME/.poetry/bin
       [ -d "$HOME/go/bin" ] && export PATH=$PATH:$HOME/go/bin
-      [ -d "$HOME/.dotnet/tools" ] && export PATH=$PATH:$HOME/.dotnet/tools
     '';
   };
 }
