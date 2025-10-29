b582c243e73d183852562a390bb0834af2bba6d0
Victor Freire <victor@freire.dev.br>
2025-10-29 12:31:15 -0300

    refactor: new git module options

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -1,6 +1,21 @@
 { pkgs, ... }:
 
 {
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      features = "side-by-side line-numbers decorations";
+      delta = {
+        navigate = true;
+      };
+      line-numbers = {
+        line-numbers-minus-style = 124;
+        line-numbers-plus-style = 28;
+      };
+    };
+  };
+
   programs.git = {
     enable = true;
     package = pkgs.gitFull;
@@ -22,29 +37,24 @@
       ".vscode"
     ];
 
-    aliases = {
-      ca = "commit --amend";
-      cm = "commit -m";
-      co = "checkout";
-      cu = ''!f(){ git stash && git checkout $1 && git fetch --all --prune && git pull origin $1; };f'';
-      df = "diff";
-      hist = "log --graph --pretty=format:'%Cred%h%Creset %s%C(yellow)%d%Creset %Cgreen(%cr)%Creset [%an]' --abbrev-commit --date=relative";
-      ri = "rebase --interactive --autosquash";
-      squash-all = ''!f(){ git reset $(git commit-tree HEAD^{tree} -m "''${1:-A new start}");};f'';
-      st = "status --short --branch";
-    };
-
-    delta = {
-      enable = true;
-      options = {
-        features = "side-by-side line-numbers decorations";
-        delta = {
-          navigate = true;
-        };
-        line-numbers = {
-          line-numbers-minus-style = 124;
-          line-numbers-plus-style = 28;
-        };
+    settings = {
+      alias = {
+        ca = "commit --amend";
+        cm = "commit -m";
+        co = "checkout";
+        cu = ''!f(){ git stash && git checkout $1 && git fetch --all --prune && git pull origin $1; };f'';
+        df = "diff";
+        hist = "log --graph --pretty=format:'%Cred%h%Creset %s%C(yellow)%d%Creset %Cgreen(%cr)%Creset [%an]' --abbrev-commit --date=relative";
+        ri = "rebase --interactive --autosquash";
+        squash-all = ''!f(){ git reset $(git commit-tree HEAD^{tree} -m "''${1:-A new start}");};f'';
+        st = "status --short --branch";
+      };
+      rerere.enabled = true;
+      merge = {
+        conflictstyle = "diff3";
+      };
+      github = {
+        user = "ratsclub";
       };
     };
 
@@ -64,15 +74,5 @@
         };
       }
     ];
-
-    extraConfig = {
-      rerere.enabled = true;
-      merge = {
-        conflictstyle = "diff3";
-      };
-      github = {
-        user = "ratsclub";
-      };
-    };
   };
 }
