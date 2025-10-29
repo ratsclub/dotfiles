3d5953331f7a155134967d2f0bfbb3721338c10f
Victor Freire <victor@freire.dev.br>
2025-10-29 12:31:31 -0300

    overlay: remove rust-analyzer overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,32 +10,14 @@ in
   default = final: _prev: import ../pkgs { pkgs = final; };
   modifications = final: prev:
     let
+      inherit (final) lib stdenv;
       inherit (final.lib.attrsets) recursiveUpdate;
       inherit (final.vscode-utils) buildVscodeMarketplaceExtension;
     in
     {
-      typos = prev.typos.overrideAttrs (_old: rec {
-        pname = "typos";
-        version = "1.38.1";
-
-        src = final.fetchFromGitHub {
-          owner = "crate-ci";
-          repo = "typos";
-          tag = "v${version}";
-          hash = "sha256-xr3k3wx9EWKm00kt1GxE31Mw5wa3N3VJJCKaUbQa4ic=";
-        };
-
-        doCheck = false;
-
-        cargoDeps = final.rustPlatform.fetchCargoVendor {
-          inherit src;
-          hash = "sha256-2XgnCXYqBvx7LRWaPt4iXznIXIEzYBlWMXbwEVZyGA8=";
-        };
-
-      });
       vscode-extensions = recursiveUpdate prev.vscode-extensions
         {
-          ionide.ionide-fsharp = buildVscodeMarketplaceExtension {
+          inko.inko-lang = buildVscodeMarketplaceExtension {
             mktplcRef = {
               name = "inko";
               publisher = "inko-lang";
