58e506577f81d81986d300afa844194512b72ff1
Victor Freire <victor@freire.dev.br>
2026-06-14 22:25:08 -0300

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     small.url = "github:nixos/nixpkgs/nixos-unstable-small";
-    stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    stable.url = "github:nixos/nixpkgs/nixos-26.05";
 
     devenv = {
       url = "github:cachix/devenv";
