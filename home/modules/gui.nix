7925509fa020b7b1fddebd150fc605fa47c83434
Victor Freire <victor@freire.dev.br>
2026-06-30 12:37:29 -0300

    gui: pycharm-professional -> pycharm

--- home/modules/gui.nix
+++ home/modules/gui.nix
@@ -3,7 +3,7 @@
 {
   home.packages = with pkgs; [
     jetbrains.rider
-    jetbrains.pycharm-professional
+    jetbrains.pycharm
     jetbrains.goland
     jetbrains.rust-rover
     jetbrains.idea-ultimate
