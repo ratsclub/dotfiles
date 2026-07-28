7795744b36dfd5c13c6bfeee3df85122ec7e9a24
ratsclub <hi@r6b.dev>
2026-07-28 14:21:38 -0300

    pkgs/claude-plugins-official: 0-unstable-2026-07-19 -> 0-unstable-2026-07-26 (#19)
    Automated package update.
    
    Co-authored-by: Forgejo Actions <noreply@r6b.dev>
    Reviewed-on: https://src.r6b.dev/ratsclub/dotfiles/pulls/19

--- pkgs/claude-plugins-official/default.nix
+++ pkgs/claude-plugins-official/default.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "claude-plugins-official";
-  version = "0-unstable-2026-07-19";
+  version = "0-unstable-2026-07-26";
 
   src = pkgs.fetchFromGitHub {
     owner = "anthropics";
     repo = "claude-plugins-official";
-    rev = "b0b9f02b0581696da41e20d6c536ec639b44080f";
-    hash = "sha256-1ufjdEbf0+FyGHYThrTwbL7kFEnxEV9uzuhHVaurGOU=";
+    rev = "4acf1a2a30144b9c426bc27d2c341507a91c3b33";
+    hash = "sha256-rrL30AYkUs4N2q1TDMFG6WOJi3moF2Chr5GGH7k6RC0=";
   };
 
   dontConfigure = true;
