12e666293dc91aec7906df642815c39d5c6debd2
ratsclub <hi@r6b.dev>
2026-07-28 14:21:30 -0300

    pkgs/claude-anthropic-skills: 0-unstable-2026-07-17 -> 0-unstable-2026-07-24 (#18)
    Automated package update.
    
    Co-authored-by: Forgejo Actions <noreply@r6b.dev>
    Reviewed-on: https://src.r6b.dev/ratsclub/dotfiles/pulls/18

--- pkgs/claude-anthropic-skills/default.nix
+++ pkgs/claude-anthropic-skills/default.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "anthropic-skills";
-  version = "0-unstable-2026-07-17";
+  version = "0-unstable-2026-07-24";
 
   src = pkgs.fetchFromGitHub {
     owner = "anthropics";
     repo = "skills";
-    rev = "fa0fa64bdc967915dc8399e803be67759e1e62b8";
-    hash = "sha256-QZ+zJkyLd/42rxgtJEZSUOz9R75Tse6UXW7G0nOkFS8=";
+    rev = "b29e7cf65e5cb78a5ac33d582270551bc74a14eb";
+    hash = "sha256-RH2B03gj4kzw1j5LORezgUZPPu8mW+mWb+Kl2U7WUbY=";
   };
 
   dontConfigure = true;
