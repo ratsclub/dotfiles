11d04de3e8a27953b5971d2459a4c0798a0fdc1d
ratsclub <hi@r6b.dev>
2026-08-10 13:23:59 -0300

    pkgs/reasonix: 1.19.6 -> 1.21.3 (#40)
    Automated package update.
    
    Co-authored-by: Forgejo Actions <noreply@r6b.dev>
    Reviewed-on: https://src.r6b.dev/ratsclub/dotfiles/pulls/40

--- pkgs/reasonix/default.nix
+++ pkgs/reasonix/default.nix
@@ -2,13 +2,13 @@
 
 pkgs.buildGoModule (finalAttrs: {
   pname = "reasonix";
-  version = "1.19.6";
+  version = "1.21.3";
 
   src = pkgs.fetchFromGitHub {
     owner = "esengine";
     repo = "DeepSeek-Reasonix";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-DcJIrNaXkeVDgLfr3FTupiGb4ruY4YRB62CLm0xuKUE=";
+    hash = "sha256-xRiYAcVXb+phAUq5khKVLFN+qANoHOPbMLTUPuVJcBA=";
   };
 
   vendorHash = "sha256-Byt7/DbSHZ+PJ8evWARRQHds/kyuydTyYH98pFwAxNY=";
