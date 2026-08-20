feef6611473f364c60e549a0511fc2313a577968
ratsclub <hi@r6b.dev>
2026-08-19 22:01:00 -0300

    pkgs/claude-shopify-ai-toolkit: 1.2.2-unstable-2026-08-05 -> 1.2.2-unstable-2026-08-19 (#62)
    Automated package update.
    
    Co-authored-by: Forgejo Actions <noreply@r6b.dev>
    Reviewed-on: https://src.capivaras.dev/ratsclub/dotfiles/pulls/62

--- pkgs/claude-shopify-ai-toolkit/default.nix
+++ pkgs/claude-shopify-ai-toolkit/default.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "shopify-ai-toolkit";
-  version = "1.2.2-unstable-2026-08-05";
+  version = "1.2.2-unstable-2026-08-19";
 
   src = pkgs.fetchFromGitHub {
     owner = "Shopify";
     repo = "Shopify-AI-Toolkit";
-    rev = "cc5af6505c27939222072449278f6356857cb064";
-    hash = "sha256-GTc79LMA6ZE2zCi9LJ4zBp/1BLNK3slCQTpklrBmjIQ=";
+    rev = "51a7d6cbed88fec14658f4e2d243dcdab128cf77";
+    hash = "sha256-NVId2URTdCIpEoefBFgErPFVrsJ3sZTnnb8xmJ2R6zQ=";
   };
 
   dontConfigure = true;
