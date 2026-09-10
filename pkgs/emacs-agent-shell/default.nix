f000fc97fca21d78402680442952bbdf3a1e2ee3
ratsclub <hi@r6b.dev>
2026-09-10 14:28:57 -0300

    pkgs/emacs-agent-shell: 0.74.3 -> 0.75.2 (#89)
    Automated package update.
    
    Co-authored-by: Forgejo Actions <noreply@capivaras.dev>
    Reviewed-on: https://src.capivaras.dev/ratsclub/dotfiles/pulls/89

--- pkgs/emacs-agent-shell/default.nix
+++ pkgs/emacs-agent-shell/default.nix
@@ -6,14 +6,14 @@
 
 emacsPackages.trivialBuild (finalAttrs: {
   pname = "agent-shell";
-  version = "0.74.3";
+  version = "0.75.2";
 
   src = pkgs.fetchFromForgejo {
     domain = "src.capivaras.dev";
     owner = "vendor";
     repo = "xenodium.agent-shell";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-2eUny9VWtb1XqLrw1uQISb32jMiFL2ESDP2DhAOb2yE=";
+    hash = "sha256-GSEW9LdzPalikaHlfxEhBcQkaeDvWdGAdUbyjIU7P1g=";
   };
 
   packageRequires = [
