d7c13aec514206555e1f5e87ef484e933cfb5628
ratsclub <hi@r6b.dev>
2026-09-14 13:29:19 -0300

    pkgs/emacs-shell-maker: 0.97.2 -> 0.97.3 (#109)
    Automated package update.
    
    Co-authored-by: Forgejo Actions <noreply@capivaras.dev>
    Reviewed-on: https://src.capivaras.dev/ratsclub/dotfiles/pulls/109

--- pkgs/emacs-shell-maker/default.nix
+++ pkgs/emacs-shell-maker/default.nix
@@ -6,14 +6,14 @@
 
 emacsPackages.trivialBuild (finalAttrs: {
   pname = "shell-maker";
-  version = "0.97.2";
+  version = "0.97.3";
 
   src = pkgs.fetchFromForgejo {
     domain = "src.capivaras.dev";
     owner = "vendor";
     repo = "xenodium.shell-maker";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-+bqe2Ss879Dj5iypzL4jRH+UEGQ/9HVxU7qIUAG8NI8=";
+    hash = "sha256-wH0OYeKthy+V0pWX1WNM8BEJW/gkzEdj/duJfRScS0w=";
   };
 
   postPatch = ''
